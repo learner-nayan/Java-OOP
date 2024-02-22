83331579a77864941b60ef23e754f7d9c1cd8647
Nayan <nayan@Nayans-MacBook-Air.local>
2024-02-22 22:26:47 +0545

    qsn no. 12 , didnot understand question

---
+++ Twelve.java
@@ -0,0 +1,7 @@
+// 12. Write a java program to use command-line arguments.
+
+class Twelve{
+    public static void main(String[] args){
+        System.out.println("Did not understand the question:\n\t12. Write a java program to use command-line arguments.");
+    }
+}
