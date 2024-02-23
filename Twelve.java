b141ed58c966a5c5c9d6471fdeb22b64b76f40a4
Nayan <nayan@Nayans-MacBook-Air.local>
2024-02-23 11:40:30 +0545

    java command line arguments

--- Twelve.java
+++ Twelve.java
@@ -2,6 +2,14 @@
 
 class Twelve{
     public static void main(String[] args){
-        System.out.println("Did not understand the question:\n\t12. Write a java program to use command-line arguments.");
+//        System.out.println("Did not understand the question:\n\t12. Write a java program to use command-line arguments.");
+        String name = args[0];
+        String surname = args[1];
+
+        System.out.println("Name: " + args[0] + "\nSurname: " + args[1]);
+
+
+        System.out.println("Name: " + name + "\nSurname: " + surname);
+
     }
 }
